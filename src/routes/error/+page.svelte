e07d5fec0081262905bf0779284230fe62df33be
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-09-28 20:46:56 +0900

    test form validate

--- src/routes/error/+page.svelte
+++ src/routes/error/+page.svelte
@@ -3,9 +3,17 @@
   <meta name="description" content="問い合わせの送信が失敗しました" />
 </svelte:head>
 
-<div class="Result">
-  <h1>Error!</h1>
-  <div>
-    <a href="/contact">Back to Previous Page</a>
-  </div>
-</div>
+<main>
+  <section>
+      <div class="container">
+          <div class="blog-detail">
+            <div class="Result">
+              <h1>Error!</h1>
+              <div>
+                <a href="/">Back to Previous Page</a>
+              </div>
+            </div>
+          </div>
+        </div>
+  </section>
+</main>
