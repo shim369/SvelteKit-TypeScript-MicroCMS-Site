47b52eb1969ff7816bb9327fe503f79dd9e46541
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-09-29 18:34:50 +0900

    add reCAPTCHA

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -24,6 +24,9 @@
             <button type="submit">Submit</button>
         </div>
     </form>
+    <p class="reCAPTCHA">This site is protected by reCAPTCHA and the Google<br>
+        <a href="https://policies.google.com/privacy">Privacy Policy</a> and
+        <a href="https://policies.google.com/terms">Terms of Service</a> apply.</p>
     </div>
 </section>
 
